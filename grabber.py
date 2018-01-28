5b1c9aaec842e3134c70828fcc054c96338cdc5b
James Campbell <james@jamescampbell.us>
2018-01-28 11:09:03 -0500

    initial push

---
+++ grabber.py
@@ -0,0 +1,12 @@
+# What: A script to grab itunes items for sale for market research and data research purposes
+# Author: James Campbell
+# License: Please see the license file in this repo
+# First Create Date: 28-Jan-2018
+# Requirements: minimal. check requirements.txt and run pip/pip3 install -f requirements.txt 
+
+import requests
+
+# globals
+itunes_url_endpoint = 'https://itunes.apple.com/search?term={}&country=us&entity={}'
+r = request.get()
+
