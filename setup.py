34c092dabedcd2730c5e8542b12f712f29d9bfe1
James Campbell <james@jamescampbell.us>
2018-01-28 18:45:23 -0500

    added classifier for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 setup(
   name = 'itunizer',
   packages = ['itunizer'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'Machine readable data from iTunes store for market research and data analytics',
   author = 'James Campbell',
+  license='GPLv3',
   author_email = 'james@jamescampbell.us',
   url = 'https://github.com/jamesacampbell/itunizer', # use the URL to the github repo
   download_url = 'https://github.com/jamesacampbell/itunizer/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['itunes', 'data-analysis', 'api'], # arbitrary keywords
-  classifiers = [],
+  classifiers = ["Programming Language :: Python :: 3 :: Only"],
 )
+
