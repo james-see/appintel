09eaf95bbfc7be377aa39131238290014e82c460
James Campbell <james@jamescampbell.us>
2018-01-28 11:27:39 -0500

    updates to get it setup as a pypi module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'itunizer',
+  packages = ['itunizer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Machine readable data from iTunes store for market research and data analytics',
+  author = 'James Campbell',
+  author_email = 'james@jamescampbell.us',
+  url = 'https://github.com/jamesacampbell/itunizer', # use the URL to the github repo
+  download_url = 'https://github.com/jamesacampbell/itunizer/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['itunes', 'data-analysis', 'api'], # arbitrary keywords
+  classifiers = [],
+)
