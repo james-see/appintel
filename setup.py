72f8761add6b6c540a66016147706fb7bd69b416
James Campbell <james@jamescampbell.us>
2018-01-28 19:12:48 -0500

    updated to get working with setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from distutils.core import setup
-setup(
-  name = 'itunizer',
-  packages = ['itunizer'], # this must be the same as the name above
-  version = '0.4',
-  description = 'Machine readable data from iTunes store for market research and data analytics',
-  author = 'James Campbell',
-  license='GPLv3',
-  author_email = 'james@jamescampbell.us',
-  url = 'https://github.com/jamesacampbell/itunizer', # use the URL to the github repo
-  download_url = 'https://github.com/jamesacampbell/itunizer/archive/0.2.tar.gz', # I'll explain this in a second
-  keywords = ['itunes', 'data-analysis', 'api'], # arbitrary keywords
-  classifiers = ["Programming Language :: Python :: 3 :: Only"],
-)
+from setuptools import setup, find_packages
 
+setup(
+    name='itunizer',
+    author='James Campbell',
+    author_email='james@#jamescampbell.us',
+    version='0.5.0',
+    license='GPLv3',
+    description = 'Machine readable data from iTunes store for market research and data analytics',
+    packages=find_packages(),
+    py_modules=['itunizer'],
+    keywords = ['itunes', 'data-analysis', 'api'], # arbitrary keywords
+    classifiers = ["Programming Language :: Python :: 3 :: Only"],
+    install_requires=[
+        'argparse',
+        'pandas',
+        'pprint',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'itunizer = itunizer.grabber:main',
+        ],
+        },
+    url = 'https://github.com/jamesacampbell/itunizer', # use the URL to the github repo
+    download_url = 'https://github.com/jamesacampbell/itunizer/archive/0.2.tar.gz', # I'll explain this in a second
+)
