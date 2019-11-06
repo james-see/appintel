ac17c85df67d11f7a31632716f9f947a7cbe7f66
James Campbell <jcampbell@primer.ai>
2019-11-05 23:18:24 -0500

    added black formatting

--- setup.py
+++ setup.py
@@ -2,33 +2,24 @@ from setuptools import setup, find_packages
 import re
 
 version = re.search(
-    '^__version__\s*=\s*"(.*)"',
-    open('itunizer/itunizer.py').read(),
-    re.M
-    ).group(1)
+    '^__version__\s*=\s*"(.*)"', open("itunizer/itunizer.py").read(), re.M
+).group(1)
 
 setup(
-    name='itunizer',
-    author='James Campbell',
-    author_email='james@jamescampbell.us',
+    name="itunizer",
+    author="James Campbell",
+    author_email="james@jamescampbell.us",
     version=version,
-    license='GPLv3',
-    description = 'Machine readable data from iTunes store for market research and data analytics',
-    packages=['itunizer'],
-    py_modules=['itunizer'],
-    keywords = ['itunes', 'data-analysis', 'api', 'market-research', 'pricing'],
-    classifiers = ["Programming Language :: Python :: 3 :: Only"],
-    install_requires=[
-        'argparse',
-        'pandas',
-        'pprint',
-        'requests'
-    ],
-    entry_points={
-        'console_scripts': [
-            'itunizer = itunizer.itunizer:main',
-        ],
-        },
-    url = 'https://github.com/jamesacampbell/itunizer',
-    download_url = 'https://github.com/jamesacampbell/itunizer/archive/{}.tar.gz'.format(version)
+    license="GPLv3",
+    description="Machine readable data from iTunes store for market research and data analytics",
+    packages=["itunizer"],
+    py_modules=["itunizer"],
+    keywords=["itunes", "data-analysis", "api", "market-research", "pricing"],
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    install_requires=["argparse", "pandas", "pprint", "requests"],
+    entry_points={"console_scripts": ["itunizer = itunizer.itunizer:main"]},
+    url="https://github.com/jamesacampbell/itunizer",
+    download_url="https://github.com/jamesacampbell/itunizer/archive/{}.tar.gz".format(
+        version
+    ),
 )
